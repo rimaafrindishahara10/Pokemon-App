e6a634742f12f3f9f608995626e7a195060fc27d
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2024-10-23 23:27:15 -1200

    Pokemon-list component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
+import { PokemonListComponent } from './component/pokemon-list/pokemon-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {
+        path:'',
+        redirectTo: 'pokemon-list',
+        pathMatch: 'full',
+    },
+    {
+    path: 'pokemon-list',
+    component:PokemonListComponent,
+   },
+
+   {
+    path: '**',
+    redirectTo: 'pokemon-list',
+   }
+];
