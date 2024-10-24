ea4a871da4679ecb62d5dd1d870e7c053c93db2b
Rima Afrin Dishahara <102999055+rimaafrindishahara10@users.noreply.github.com>
2024-10-23 19:51:58 -1200

    Footer component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopBannerComponent } from './component/top-banner/top-banner.component';
+import { FooterComponent } from "./component/footer/footer.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , TopBannerComponent],
+  imports: [RouterOutlet, TopBannerComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
